8c0de2cece6e3d961cd61cc4f852d9ad289cc0c3
tysonchae <tysonchae@gmail.com>
2017-12-23 05:42:42 -0800

    initialize

---
+++ src/main/java/me/tyson/AmugonaagainApplication.java
@@ -0,0 +1,12 @@
+package me.tyson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmugonaagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmugonaagainApplication.class, args);
+	}
+}
